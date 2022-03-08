e5de22506d6ea06c3b76ec0b1401c2dbabe63fb4
Helton Rios <helton.placido@gmail.com>
2022-03-08 00:28:02 -0300

    Initial commit

---
+++ script.js
@@ -0,0 +1,17 @@
+document
+  .querySelector('.hamburguer')
+  .addEventListener('click', () =>
+    document.querySelector('.menu').classList.toggle('hidden')
+  )
+
+document
+  .querySelector('.hamburguer')
+  .addEventListener('click', () =>
+    document.querySelector('.menu-burguer-open').classList.toggle('hidden')
+  )
+
+document
+  .querySelector('.hamburguer')
+  .addEventListener('click', () =>
+    document.querySelector('.menu-burguer-close').classList.toggle('hidden')
+  )
